159df5c9ca2b3479d8b6c5d7a7ff603f26490166
TulaByte <83277885+tula-byte@users.noreply.github.com>
2022-09-26 14:29:20 +1000

    Fixed navigation bugs and added custom 404 page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./dist/**/*.{html,js}"],
+  content: ["./dist/**/*.{html,js}", "./404.html"],
   theme: {
     extend: {
       fontFamily: {
         gambetta: ["gambetta", "serif"],
       },
+      animation: {
+        'pulse-slow': 'pulse 4s cubic-bezier(0.4, 0, 0.6, 1) infinite;'
+      }
     },
   },
   plugins: [
